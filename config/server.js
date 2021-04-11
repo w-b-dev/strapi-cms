108d1575aa9a47385904d6c7d01df655c449d6bf
Daniel Santos <daniel@codein.ca>
2021-04-11 18:41:01 -0300

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '508873ee582fdf0383ef6a48cf29a19b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '999c7b22-27f5-4789-8b79-626ce5cc9c62'
+};
